784f8b666caaa5a85f43d37ba854b7a66fd55957
Jesufemi-A <jesufemia1@gmail.com>
2025-11-04 22:55:35 +0100

    added global error handler and middle for handling unhandled routes

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const productsRouter = require("./routes/productsRouter");
+const userRouter = require("./routes/userRouter");
+const AppError = require("./errorHandler/appError");
+const globalErrorHandler = require("./errorHandler/globalErrorHandler");
 
 const app = express();
 
+app.use(express.json());
+
 /* Routes */
+app.use("/api/v1/products", productsRouter);
+app.use("api/v1/auth", userRouter);
 
-app.use("/api/v1", productsRouter);
+app.all("/{*any}", (req, res, next) => {
+  next(new AppError(`Can not find ${req.originalUrl} on this server!`, 404));
+});
 
+app.use(globalErrorHandler);
 module.exports = app;
