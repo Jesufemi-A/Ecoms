2e1d0625fabeee610dbedbc8f38dab3a652ac0e9
Jesufemi-A <jesufemia1@gmail.com>
2025-11-05 22:46:29 +0100

    refactored login function

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const User = require("./../models/userModel");
 const jwt = require("jsonwebtoken");
 
 const dotenv = require("dotenv");
+const AppError = require("../errorHandler/appError");
 
 exports.signup = async (req, res, next) => {
   try {
@@ -27,6 +28,31 @@ exports.signup = async (req, res, next) => {
         user: newUser,
       },
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const currentUser = await User.findOne({ email: req.body.email });
+    // console.log(currentUser);
+    if (currentUser) {
+      const token = jwt.sign(
+        { id: currentUser.id, email: currentUser.email },
+        process.env.SECRET_KEY
+      );
+      // console.log(currentUser.id);
+      res.status(200).json({
+        status: "Login Successful",
+        data: {
+          token,
+          user: currentUser,
+        },
+      });
+    } else if (!currentUser) {
+      throw new AppError("Incorrect email or password", 404);
+    }
   } catch (error) {
     next(error);
   }
