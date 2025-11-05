833d44c54d5ce35240df344a9ec886f46d19d84b
Jesufemi-A <jesufemia1@gmail.com>
2025-11-05 18:19:05 +0100

    modified signup function to use jwt authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,31 @@
 const bcrypt = require("bcryptjs");
 const User = require("./../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const dotenv = require("dotenv");
 
 exports.signup = async (req, res, next) => {
   try {
     const newUser = new User(req.body);
+
+    newUser.password = await bcrypt.hash(
+      newUser.password,
+      parseInt(process.env.SALT)
+    );
+    newUser.confirmPassword = undefined;
     await newUser.save();
 
+    const token = jwt.sign(
+      { userId: newUser.userId, userEmail: newUser.userEmail },
+      process.env.SECRET_KEY
+    );
+
     res.status(201).json({
-      status: "success",
-      data: newUser,
+      status: "sign up successful",
+      data: {
+        token,
+        user: newUser,
+      },
     });
   } catch (error) {
     next(error);
