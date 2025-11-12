69fa9274c048abee3c2c25bf121fc1277c5ce01e
Jesufemi-A <jesufemia1@gmail.com>
2025-11-12 16:30:24 +0100

    moved config loading to the top of file

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const { MongoClient } = require("mongodb");
 const dotenv = require("dotenv");
-const app = require("./app");
-
 dotenv.config({
   path: "./config.env",
 });
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = require("./app");
 
 const port = process.env.PORT || 3000;
 const uri = process.env.DATABASE_LOCAL;
@@ -15,7 +14,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(uri);
     console.log("Connected successfully to MongoDB! Database is running");
-    console.log(` MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
@@ -27,3 +26,5 @@ connectDB();
 app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
 });
+
+// console.log(process.env.NODE_ENV)
