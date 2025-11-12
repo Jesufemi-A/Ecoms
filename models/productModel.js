c2a08e63742ef6377165d09f4c6c6728a86cd4b2
Jesufemi-A <jesufemia1@gmail.com>
2025-11-12 18:00:18 +0100

    product model

--- models/productModel.js
+++ models/productModel.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  review: { type: String, required: false },
-  productImage: {
-    type: String,
-    required: false,
-  },
+  name: { type: String, required: [true, "Please, enter the product name"] },
+  price: { type: Number, required: [true, "price must be a number"] },
+  imageUrl: { type: String, required: [true, "product needs a product image"] },
 });
 
 const Products = mongoose.model("Products", productSchema);
+
+module.exports = Products;
