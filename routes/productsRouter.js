c38e248b00d1e4677b018d2c96b209a9600d0ea1
Jesufemi-A <jesufemia1@gmail.com>
2025-11-12 17:15:17 +0100

    add route to retrive a single product resource

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const productController = require("./../controllers/productController");
 const authcontroller = require("./../controllers/authController");
+const uploadSingle = require("./../middleware/multerMiddleware");
+
 const router = express.Router();
 
 router
   .route("/")
   .get(
     authcontroller.protectRoute,
-    authcontroller.restrictTo("user"),
+    authcontroller.restrictTo(["admin", "user"]),
     productController.getProducts
   )
   .post(
     authcontroller.protectRoute,
     authcontroller.restrictTo("admin"),
+    uploadSingle,
     productController.createProduct
   );
 
+router.route("/:id").get(productController.getProduct);
 // router.route("/:productId").patch(productController.updateProduct);
 
 module.exports = router;
