40dd7e9ac4071c3a99577773b5a4849dd27a20a0
Jesufemi-A <jesufemia1@gmail.com>
2025-11-14 15:38:36 +0100

    cartrouter

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const productsRouter = require("./routes/productsRouter");
 const userRouter = require("./routes/userRouter");
 const AppError = require("./errorHandler/appError");
 const globalErrorHandler = require("./errorHandler/globalErrorHandler");
+const cartRouter = require("./routes/cartRouter");
 
 const app = express();
 
@@ -10,7 +11,8 @@ app.use(express.json());
 
 /* Routes */
 app.use("/api/v1/products", productsRouter);
-app.use("api/v1/auth", userRouter);
+app.use("/api/v1/auth", userRouter);
+app.use("/api/v1/cart", cartRouter);
 
 app.all("/{*any}", (req, res, next) => {
   next(new AppError(`Can not find ${req.originalUrl} on this server!`, 404));
