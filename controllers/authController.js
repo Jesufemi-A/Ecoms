0343943ae5aad1bc53c32989744c85f2547637bd
Jesufemi-A <jesufemia1@gmail.com>
2025-11-16 02:30:54 +0100

    implemented Google OAuth

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,9 +2,27 @@ const { promisify } = require("util");
 const bcrypt = require("bcryptjs");
 const User = require("./../models/userModel");
 const jwt = require("jsonwebtoken");
+const axios = require("axios");
+const { OAuth2Client } = require("google-auth-library");
 
 const dotenv = require("dotenv");
 const AppError = require("../errorHandler/appError");
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+async function verifyGoogleIdToken(idToken) {
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: idToken,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    const payload = ticket.getPayload();
+    return payload;
+  } catch (error) {
+    return null;
+  }
+}
 
 const verifyPassword = async (passwordInput, passwordHash) => {
   return bcrypt.compare(passwordInput, passwordHash);
@@ -86,22 +104,17 @@ exports.protectRoute = async (req, res, next) => {
     }
     if (!token) throw new AppError("You are not logged in. Please log in", 401);
 
-    // console.log("Token " + token);
     const verifyToken = await promisify(jwt.verify)(
       token,
       process.env.SECRET_KEY
     );
 
-    // console.log("verify ------------" + verifyToken);
     if (!verifyToken) {
       throw new AppError("Please log in...", 401);
     }
 
     const currentUser = await User.findById(verifyToken.id);
-    // console.log(
-    //   "currentUser -----" + currentUser,
-    //   "verifyToken -----" + verifyToken
-    // );
+
     if (!currentUser) {
       throw new AppError(
         "The user belonging to this token no longer exist",
@@ -110,7 +123,6 @@ exports.protectRoute = async (req, res, next) => {
     }
 
     req.user = currentUser;
-    // console.log("current User -------" + currentUser);
 
     next();
   } catch (error) {
@@ -118,15 +130,90 @@ exports.protectRoute = async (req, res, next) => {
   }
 };
 
-// const fn = (role) => async(req, res, next);
-
 exports.restrictTo =
   (...roles) =>
   (req, res, next) => {
-    console.log(req.user.role);
     if (!roles.includes(req.user.role)) {
       next(new AppError("You do not have permission to this route", 403));
     } else {
       next();
     }
   };
+
+exports.googleAuth = (req, res) => {
+  const redirectUrl =
+    "https://accounts.google.com/o/oauth2/v2/auth?" +
+    new URLSearchParams({
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      redirect_uri: process.env.GOOGLE_REDIRECT_URI,
+      response_type: "code",
+      scope: "openid email profile",
+      prompt: "select_account",
+    }).toString();
+  res.redirect(redirectUrl);
+};
+
+exports.googleCallback = async (req, res, next) => {
+  try {
+    const code = req.query.code;
+
+    if (!code) {
+      return next(new AppError("No code return from Google", 400));
+    }
+
+    const tokenResponse = await axios.post(
+      "https://oauth2.googleapis.com/token",
+      new URLSearchParams({
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        redirect_uri: process.env.GOOGLE_REDIRECT_URI,
+        grant_type: "authorization_code",
+        code,
+      }),
+      { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+    );
+
+    const { id_token, access_token } = tokenResponse.data;
+
+    const googleUser = await verifyGoogleIdToken(id_token);
+
+    if (!googleUser)
+      return next(new AppError("Google ID Token verification failed"), 404);
+
+    const [firstName, ...rest] = googleUser.name.split(" ");
+
+    const lastName = rest.join(" ");
+
+    let user = await User.findOne({
+      email: googleUser.email,
+    });
+
+    if (!user) {
+      user = await User.create({
+        email: googleUser.email,
+        googleId: googleUser.sub,
+        firstName,
+        lastName,
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.SECRET_KEY,
+      { expiresIn: process.env.EXPIRES_IN }
+    );
+
+    res.status(200).json({
+      status: "success",
+      message: "Google login successful",
+      token,
+      data: {
+        user,
+      },
+    });
+  } catch (error) {
+    next(new AppError(error.message, 500));
+  }
+};
